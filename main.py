8c262ab898eccec06af004cfb558306dc38c483e
Dimitris Chatzigiannis <dimitris.chatzigiannis@gmail.com>
2024-01-07 03:22:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class People:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __call__(self, age):
+        print(f"{self.name} {self.surname} is {age} years old...")
+
+
+if __name__ == "__main__":
+    print("Starting code execution")
+
+    Dimitris = People(name="Dimitris", surname="Chatzigiannis")
+
+    Dimitris(38)
